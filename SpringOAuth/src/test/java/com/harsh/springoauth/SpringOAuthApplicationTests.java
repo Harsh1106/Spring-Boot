2990aafa7304d6e3cb84a59ffbf971ae07079548
harsh <sinha.harshraj1101@gmail.com>
2025-09-19 12:22:30 +0530

    Implemented OAuth2 login

---
+++ SpringOAuth/src/test/java/com/harsh/springoauth/SpringOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.springoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
